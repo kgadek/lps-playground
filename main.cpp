056f827f37f27c9a3a4e3070a5c04ded0d3917ed
Konrad Gądek <kgadek@gmail.com>
2016-09-13 17:33:46 +0200

    main: run LPS from mmap-ed file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+#include <string>
+
+#include <errno.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
 #include "lps.hpp"
 
-int main()
+
+int main(int argc, char **argv)
 {
-    std::cout << "OHAI\n";
+    assert(argc == 2);
+
+    int fd = open(argv[1], O_RDONLY);
+    assert(fd);
+
+    struct stat fd_stat;
+    assert(fstat(static_cast<int>(fd), &fd_stat) == 0);
+    assert(S_ISREG(fd_stat.st_mode));
+
+    void* s = mmap(0, fd_stat.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    assert(s != MAP_FAILED);
+    assert(close(fd) == 0);
+
+    auto result = lps(static_cast<const char*>(s), fd_stat.st_size);
+    std::string result_substr {result.first, result.second}; 
+    std::cout << result_substr << std::endl;
+
+    assert(munmap(s, fd_stat.st_size) == 0);
+    return 0;
 }
