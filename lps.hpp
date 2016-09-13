d4c40a3aacd52ef3c6bee660c9d733cac07d5eb4
Konrad Gądek <kgadek@gmail.com>
2016-09-13 03:21:10 +0200

    lps works (for odd palindromes)

--- lps.hpp
+++ lps.hpp
@@ -3,13 +3,52 @@
 #include <cstring>
 #include <algorithm>
 #include <iterator>
+#include <utility>
+
+#include <iostream>
 
 
-// based on https://www.rosettacode.org/wiki/Palindrome_detection#C.2B.2B
-inline bool is_palindrome(const char *s, std::size_t length) {
-    using std::equal;
-    using std::reverse_iterator;
+std::pair<const char*, std::size_t>
+lps(const char* s, std::size_t s_length)
+{
 
-    return equal(s, s+length/2, reverse_iterator<const char*>{s+length});
+    const char* longest_palindrome = s;
+    std::size_t longest_length = 0;
+
+    const char* s_last = s + s_length - 1;
+
+    const char* curr_center = s;
+
+    for( ; *curr_center != '\0'; ++curr_center)
+    {
+
+        std::size_t curr_length = 1;
+        const char* left_it = curr_center;
+        const char* right_it = curr_center;
+
+        for( ; s < left_it && right_it < s_last; curr_length+=2)
+        {
+            if( *(--left_it) != *(++right_it))
+            {
+                ++left_it;
+                --right_it;
+                break;
+            }
+        }
+
+        if(curr_length > longest_length)
+        {
+            longest_palindrome = left_it;
+            longest_length = curr_length;
+        }
+    }
+
+    return std::make_pair(longest_palindrome, longest_length);
+}
+
+std::pair<const char*, std::size_t>
+inline lps(const char *s)
+{
+    return lps(s, strlen(s));
 }
 
--- t.cpp
+++ t.cpp
@@ -1,6 +1,15 @@
 #include "lps.hpp"
 #include "gmock/gmock.h"
 
+// based on https://www.rosettacode.org/wiki/Palindrome_detection#C.2B.2B
+inline bool
+is_palindrome(const char *s, std::size_t length) {
+    using std::equal;
+    using std::reverse_iterator;
+
+    return equal(s, s+length/2, reverse_iterator<const char*>{s+length});
+}
+
 
 TEST(IsPalindrome, CStringEven) {
     EXPECT_TRUE(is_palindrome("", 0));
@@ -19,6 +28,58 @@ TEST(IsPalindrome, CStringOdd) {
 
     EXPECT_FALSE(is_palindrome("ABB", 3));
     EXPECT_FALSE(is_palindrome("ABABC", 5));
+}
+
+
+::testing::AssertionResult IsCorrectLPS(const char *expected, std::pair<const char*, std::size_t> &&result) {
+    auto result_cstring = result.first;
+    auto result_len = result.second;
+    auto expected_char = expected;
+
+    std::size_t compared = 0;
+
+    for(; *expected_char != '\0'; ++expected_char, ++result_cstring, ++compared) {
+        if(*expected_char != *result_cstring) {
+            return ::testing::AssertionFailure() <<
+                "expected='" << expected << "', " <<
+                "but got '" << result.first << "' [:" << result.second << "]";
+        }
+    }
+
+    if(compared != result_len) {
+        return ::testing::AssertionFailure() << 
+                "expected='" << expected << "', " <<
+                "and got '" << result.first << "'[:" << result.second << "], (STRINGS MATCH) " <<
+                "however the reported length shall be " << strlen(expected);
+    }
+
+    if(!is_palindrome(result.first, result.second)) {
+        return ::testing::AssertionFailure() <<
+                "Whoops, everything seems good but the result is not a palindrome!";
+    }
+
+    return ::testing::AssertionSuccess();
+}
+
+#define EXPECT_LPS_EQ(expected, test_string) EXPECT_TRUE(IsCorrectLPS((expected), lps(test_string)))
+
+TEST(LPS, CString) {
+    EXPECT_LPS_EQ("", "");
+    EXPECT_LPS_EQ("A", "A");
+    EXPECT_LPS_EQ("A", "AB");
+    EXPECT_LPS_EQ("A", "ABC");
+
+    EXPECT_LPS_EQ("ABA", "ABA");
+    EXPECT_LPS_EQ("ABABA", "ABABA");
+    EXPECT_LPS_EQ("BBB", "ABBBC");
+
+    EXPECT_LPS_EQ("A", "ABCDEF");
+    EXPECT_LPS_EQ("FEDCABACDEF", "FEDCABACDEF");
+    EXPECT_LPS_EQ("ABA", "FEDC#ABACDEF");
+
+    EXPECT_LPS_EQ("A", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    EXPECT_LPS_EQ("ZZZ", "ABCZZZ");
 }
 
 
